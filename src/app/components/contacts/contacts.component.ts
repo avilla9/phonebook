50e1f64426cd577023ff9fbae663fac3c1414b4d
Armando Villanueva <armandojvilla9@gmail.com>
2022-10-06 00:08:24 -0400

    change color methods

--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -12,6 +12,7 @@ export class ContactsComponent implements OnInit {
   public contacts!: contactInterface[];
   public colors = ['#df2222', '#e77517', '#aa8700', '#4ae222', '#36ddf0', '#ff76d2', '#4a3f9a', '#d691e8'];
   public lastColor: any = this.getRandomColor();
+  public avatarColors: string[] = [];
   public dialogOptions = {
     DialogPosition: 'bottom',
     maxWidth: '100vw',
@@ -28,21 +29,31 @@ export class ContactsComponent implements OnInit {
 
   ngOnInit(): void {
     this.contacts = this.contactsService.getContacts();
+    this.setAvatarColors();
   }
 
-  getRandomColor(): any {
+  getRandomColor(): string {
     let random = Math.floor(Math.random() * this.colors.length);
     if (this.lastColor == this.colors[random]) {
       return this.getRandomColor();
     }
     this.lastColor = this.colors[random]
-    return { 'background-color': this.colors[random] };
+    return this.colors[random];
+    //return { 'background-color': this.colors[random] };
+  }
+
+  setAvatarColors() {
+    this.avatarColors = [];
+    this.contacts?.forEach(contact => {
+      this.avatarColors.push(this.getRandomColor())
+    });
   }
 
   add() {
     const dialogRef = this.dialog.open(ContactFormComponent, this.dialogOptions);
     dialogRef.afterClosed().subscribe((contact: contactInterface) => {
       this.contacts = this.contactsService.getContacts();
+      this.avatarColors.push(this.getRandomColor());
     });
   }
 }
